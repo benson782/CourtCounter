f7a4c1832417d741b83986290295353a21b8d1bc
Benson Malonzo <benson782@hotmail.com>
2015-08-10 18:19:00 -0700

    Increment score based on button pressed and display the score.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +47,18 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void addThreeForTeamA(View view){
-        displayForTeamA(3);
+    public void addThreeForTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
     }
 
-    public void addTwoForTeamA(View view){
-        displayForTeamA(2);
+    public void addTwoForTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
 
-    public void addOneForTeamA(View view){
-        displayForTeamA(1);
+    public void addOneForTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
 }
